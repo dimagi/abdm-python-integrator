b6f6241e4409ea8bdf7f7264983e54df5dd845fa
Ajeet Yadav <ayadav@dimagi.com>
2023-09-11 22:00:01 +0530

    API views for ABHA creation

--- abdm_python_integrator/abha/utils/response_util.py
+++ abdm_python_integrator/abha/utils/response_util.py
@@ -1,5 +1,16 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
+
+success_response_keys = ["txnId", "healthIdNumber", "auth_methods", "token",
+                         "health_card", "health_id", "authMethods"]
+
+
+def parse_response(response_data):
+    if not response_data:
+        return Response({"error": "No valid response found"}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    if any([key in response_data for key in success_response_keys]):
+        return _get_success_abdm_response(response_data)
+    return _get_error_abdm_response(response_data)
 
 
 def generate_invalid_req_response(message, error_code=HTTP_400_BAD_REQUEST):
@@ -12,3 +23,18 @@ def generate_invalid_req_response(message, error_code=HTTP_400_BAD_REQUEST):
         }]
     }
     return Response(resp, status=HTTP_400_BAD_REQUEST)
+
+
+def _get_success_abdm_response(response_data):
+    return Response(response_data, status=HTTP_200_OK)
+
+
+def _get_error_abdm_response(response_data):
+    if "code" in response_data:
+        return _parse_response(response_data)
+    return generate_invalid_req_response(response_data)
+
+
+def _parse_response(response_data):
+    status_code = int(response_data.get("code").split("-")[-1])
+    return Response(response_data, status=status_code)
---
+++ abdm_python_integrator/urls.py
@@ -0,0 +1,3 @@
+from abdm_python_integrator.abha.urls import abha_urls
+
+urlpatterns = abha_urls

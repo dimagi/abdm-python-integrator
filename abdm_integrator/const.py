cce4dde656e3c7a905658de5af89b34715169d95
Ajeet Yadav <ayadav@dimagi.com>
2023-09-18 21:30:19 +0530

    consent status choices

--- abdm_integrator/const.py
+++ abdm_integrator/const.py
@@ -1,1 +1,17 @@
 SESSIONS_PATH = '/v0.5/sessions'
+
+
+class ConsentStatus:
+    PENDING = 'PENDING'
+    REQUESTED = 'REQUESTED'
+    GRANTED = 'GRANTED'
+    DENIED = 'DENIED'
+    REVOKED = 'REVOKED'
+    EXPIRED = 'EXPIRED'
+    ERROR = 'ERROR'
+
+    GATEWAY_CHOICES = [(GRANTED, 'Granted'), (DENIED, 'Denied'), (REVOKED, 'Revoked'),
+                       (EXPIRED, 'Expired')]
+    CONSENT_REQUEST_CHOICES = GATEWAY_CHOICES + [(PENDING, 'Pending request from Gateway'),
+                                                     (REQUESTED, 'Requested'),
+                                                     (ERROR, 'Error occurred')]

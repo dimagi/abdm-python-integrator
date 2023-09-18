084e4b84fdc56089d1df25c9d28c2d352cce4071
Ajeet Yadav <ayadav@dimagi.com>
2023-09-18 14:04:52 +0530

    abha creation: refactor to use class based views

--- abdm_python_integrator/abha/urls.py
+++ abdm_python_integrator/abha/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from abdm_python_integrator.abha.views import abha_creation_views, abha_verification_views
 
 abha_creation_urls = [
-    path('api/generate_aadhaar_otp', abha_creation_views.generate_aadhaar_otp, name='generate_aadhaar_otp'),
-    path('api/generate_mobile_otp', abha_creation_views.generate_mobile_otp, name='generate_mobile_otp'),
-    path('api/verify_aadhaar_otp', abha_creation_views.verify_aadhaar_otp, name='verify_aadhaar_otp'),
-    path('api/verify_mobile_otp', abha_creation_views.verify_mobile_otp, name='verify_mobile_otp'),
+    path('api/generate_aadhaar_otp', abha_creation_views.GenerateAadhaarOTP.as_view(), name='generate_aadhaar_otp'),
+    path('api/generate_mobile_otp', abha_creation_views.GenerateMobileOTP.as_view(), name='generate_mobile_otp'),
+    path('api/verify_aadhaar_otp', abha_creation_views.VerifyAadhaarOTP.as_view(), name='verify_aadhaar_otp'),
+    path('api/verify_mobile_otp', abha_creation_views.VerifyMobileOTP.as_view(), name='verify_mobile_otp'),
 ]
 
 abha_verification_urls = [
--- abdm_python_integrator/abha/views/abha_creation_views.py
+++ abdm_python_integrator/abha/views/abha_creation_views.py
@@ -1,60 +1,55 @@
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from django.utils.decorators import method_decorator
 
 from abdm_python_integrator.abha.utils import abha_creation_util as abdm_util
 from abdm_python_integrator.abha.utils.decorators import required_request_params
 from abdm_python_integrator.abha.utils.response_util import parse_response
+from abdm_python_integrator.abha.views.base import ABHABaseView
 from abdm_python_integrator.settings import app_settings
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["aadhaar"])
-def generate_aadhaar_otp(request):
-    aadhaar_number = request.data.get("aadhaar")
-    raw_response = abdm_util.generate_aadhaar_otp(aadhaar_number)
-    return parse_response(raw_response)
+class GenerateAadhaarOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["aadhaar"]))
+    def post(self, request, format=None):
+        aadhaar_number = request.data.get("aadhaar")
+        raw_response = abdm_util.generate_aadhaar_otp(aadhaar_number)
+        return parse_response(raw_response)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["txn_id", "mobile_number"])
-def generate_mobile_otp(request):
-    txn_id = request.data.get("txn_id")
-    mobile_number = request.data.get("mobile_number")
-    resp = abdm_util.generate_mobile_otp(mobile_number, txn_id)
-    return parse_response(resp)
+class GenerateMobileOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["txn_id", "mobile_number"]))
+    def post(self, request, format=None):
+        txn_id = request.data.get("txn_id")
+        mobile_number = request.data.get("mobile_number")
+        resp = abdm_util.generate_mobile_otp(mobile_number, txn_id)
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["txn_id", "otp"])
-def verify_aadhaar_otp(request):
-    txn_id = request.data.get("txn_id")
-    otp = request.data.get("otp")
-    resp = abdm_util.verify_aadhar_otp(otp, txn_id)
-    return parse_response(resp)
+class VerifyAadhaarOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["txn_id", "otp"]))
+    def post(self, request, format=None):
+        txn_id = request.data.get("txn_id")
+        otp = request.data.get("otp")
+        resp = abdm_util.verify_aadhar_otp(otp, txn_id)
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["txn_id", "otp"])
-# TODO Consider adding health id as required param
-def verify_mobile_otp(request):
-    txn_id = request.data.get("txn_id")
-    otp = request.data.get("otp")
-    health_id = request.data.get("health_id")
-    resp = abdm_util.verify_mobile_otp(otp, txn_id)
-    if resp and "txnId" in resp:
-        resp = abdm_util.create_health_id(txn_id, health_id)
-        resp["user_token"] = resp.pop("token")
-        resp.pop("refreshToken")
-        resp["exists_on_abdm"] = not resp.pop("new")
-        if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
-            resp["exists_on_hq"] = (app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().
-                                    check_if_abha_registered(request.user, health_id))
-    return parse_response(resp)
+class VerifyMobileOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["txn_id", "otp"]))
+    def post(self, request, format=None):
+        txn_id = request.data.get("txn_id")
+        otp = request.data.get("otp")
+        health_id = request.data.get("health_id")
+        resp = abdm_util.verify_mobile_otp(otp, txn_id)
+        if resp and "txnId" in resp:
+            resp = abdm_util.create_health_id(txn_id, health_id)
+            resp["user_token"] = resp.pop("token")
+            resp.pop("refreshToken")
+            resp["exists_on_abdm"] = not resp.pop("new")
+            if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
+                resp["exists_on_hq"] = (app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().
+                                        check_if_abha_registered(request.user, health_id))
+        return parse_response(resp)

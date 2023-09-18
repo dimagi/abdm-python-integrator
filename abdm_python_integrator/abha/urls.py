f8f788daa75127060abc70c223ee38ab04b24ccf
Ajeet Yadav <ayadav@dimagi.com>
2023-09-18 14:23:12 +0530

    abha verification: refactor to use class based views

--- abdm_python_integrator/abha/urls.py
+++ abdm_python_integrator/abha/urls.py
@@ -3,22 +3,24 @@ from django.urls import path
 from abdm_python_integrator.abha.views import abha_creation_views, abha_verification_views
 
 abha_creation_urls = [
-    path('api/generate_aadhaar_otp', abha_creation_views.GenerateAadhaarOTP.as_view(), name='generate_aadhaar_otp'),
+    path('api/generate_aadhaar_otp', abha_creation_views.GenerateAadhaarOTP.as_view(),
+         name='generate_aadhaar_otp'),
     path('api/generate_mobile_otp', abha_creation_views.GenerateMobileOTP.as_view(), name='generate_mobile_otp'),
     path('api/verify_aadhaar_otp', abha_creation_views.VerifyAadhaarOTP.as_view(), name='verify_aadhaar_otp'),
     path('api/verify_mobile_otp', abha_creation_views.VerifyMobileOTP.as_view(), name='verify_mobile_otp'),
 ]
 
 abha_verification_urls = [
-    path('api/get_auth_methods', abha_verification_views.get_auth_methods, name='get_auth_methods'),
-    path('api/generate_auth_otp', abha_verification_views.generate_auth_otp, name='generate_auth_otp'),
-    path('api/confirm_with_mobile_otp', abha_verification_views.confirm_with_mobile_otp,
+    path('api/get_auth_methods', abha_verification_views.GetAuthMethods.as_view(), name='get_auth_methods'),
+    path('api/generate_auth_otp', abha_verification_views.GenerateAuthOTP.as_view(), name='generate_auth_otp'),
+    path('api/confirm_with_mobile_otp', abha_verification_views.ConfirmWithMobileOTP.as_view(),
          name='confirm_with_mobile_otp'),
-    path('api/confirm_with_aadhaar_otp', abha_verification_views.confirm_with_aadhaar_otp,
+    path('api/confirm_with_aadhaar_otp', abha_verification_views.ConfirmWithAadhaarOTP.as_view(),
          name='confirm_with_aadhaar_otp'),
-    path('api/search_health_id', abha_verification_views.search_health_id, name='search_health_id'),
-    path('api/get_health_card_png', abha_verification_views.get_health_card_png, name='get_health_card_png'),
-    path('api/exists_by_health_id', abha_verification_views.get_existence_by_health_id,
+    path('api/search_health_id', abha_verification_views.SearchHealthId.as_view(), name='search_health_id'),
+    path('api/get_health_card_png', abha_verification_views.GetHealthCardPng.as_view(),
+         name='get_health_card_png'),
+    path('api/exists_by_health_id', abha_verification_views.GetExistenceByHealthId.as_view(),
          name='exists_by_health_id'),
 ]
 
--- abdm_python_integrator/abha/views/abha_verification_views.py
+++ abdm_python_integrator/abha/views/abha_verification_views.py
@@ -1,102 +1,95 @@
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from django.utils.decorators import method_decorator
 
 from abdm_python_integrator.abha.exceptions import ABHA_ERROR_MESSAGES, ABHA_IN_USE_ERROR_CODE
 from abdm_python_integrator.abha.utils import abha_verification_util as abdm_util
 from abdm_python_integrator.abha.utils.abha_verification_util import get_account_information
 from abdm_python_integrator.abha.utils.decorators import required_request_params
 from abdm_python_integrator.abha.utils.response_util import generate_invalid_req_response, parse_response
+from abdm_python_integrator.abha.views.base import ABHABaseView
 from abdm_python_integrator.settings import app_settings
 
 
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-def get_auth_methods(request):
-    health_id = request.query_params.get("health_id")
-    if not health_id:
-        error_msg = "Missing required parameter: health_id"
-        return generate_invalid_req_response(error_msg)
-    resp = abdm_util.search_by_health_id(health_id)
-    auth_methods = resp.get("authMethods")
-    resp = {"auth_methods": auth_methods}
-    return parse_response(resp)
+class GetAuthMethods(ABHABaseView):
+
+    def get(self, request, format=None):
+        health_id = request.query_params.get("health_id")
+        if not health_id:
+            error_msg = "Missing required parameter: health_id"
+            return generate_invalid_req_response(error_msg)
+        resp = abdm_util.search_by_health_id(health_id)
+        auth_methods = resp.get("authMethods")
+        resp = {"auth_methods": auth_methods}
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["health_id", "auth_method"])
-def generate_auth_otp(request):
-    health_id = request.data.get("health_id")
-    auth_method = request.data.get("auth_method")
-    resp = abdm_util.generate_auth_otp(health_id, auth_method)
-    return parse_response(resp)
+class GenerateAuthOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["health_id", "auth_method"]))
+    def post(self, request, format=None):
+        health_id = request.data.get("health_id")
+        auth_method = request.data.get("auth_method")
+        resp = abdm_util.generate_auth_otp(health_id, auth_method)
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["txn_id", "otp"])
-def confirm_with_mobile_otp(request):
-    txn_id = request.data.get("txn_id")
-    otp = request.data.get("otp")
-    resp = abdm_util.confirm_with_mobile_otp(otp, txn_id)
-    if "token" in resp:
-        resp = {"status": "success", "txnId": txn_id, "user_token": resp.get("token")}
-        resp.update(get_account_information(resp.get("user_token")))
-    return parse_response(resp)
+class ConfirmWithMobileOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["txn_id", "otp"]))
+    def post(self, request, format=None):
+        txn_id = request.data.get("txn_id")
+        otp = request.data.get("otp")
+        resp = abdm_util.confirm_with_mobile_otp(otp, txn_id)
+        if "token" in resp:
+            resp = {"status": "success", "txnId": txn_id, "user_token": resp.get("token")}
+            resp.update(get_account_information(resp.get("user_token")))
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["txn_id", "otp"])
-def confirm_with_aadhaar_otp(request):
-    txn_id = request.data.get("txn_id")
-    otp = request.data.get("otp")
-    resp = abdm_util.confirm_with_aadhaar_otp(otp, txn_id)
-    if "token" in resp:
-        resp = {"status": "success", "txnId": txn_id, "user_token": resp.get("token")}
-        resp.update(get_account_information(resp.get("user_token")))
-    return parse_response(resp)
+class ConfirmWithAadhaarOTP(ABHABaseView):
+
+    @method_decorator(required_request_params(["txn_id", "otp"]))
+    def post(self, request, format=None):
+        txn_id = request.data.get("txn_id")
+        otp = request.data.get("otp")
+        resp = abdm_util.confirm_with_aadhaar_otp(otp, txn_id)
+        if "token" in resp:
+            resp = {"status": "success", "txnId": txn_id, "user_token": resp.get("token")}
+            resp.update(get_account_information(resp.get("user_token")))
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["health_id"])
-def search_health_id(request):
-    health_id = request.data.get("health_id")
-    if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
-        if (app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().
-                check_if_abha_registered(request.user, health_id)):
-            return generate_invalid_req_response(ABHA_ERROR_MESSAGES[ABHA_IN_USE_ERROR_CODE],
-                                                 error_code=ABHA_IN_USE_ERROR_CODE)
-    resp = abdm_util.search_by_health_id(health_id)
-    return parse_response(resp)
+class SearchHealthId(ABHABaseView):
+
+    @method_decorator(required_request_params(["health_id"]))
+    def post(self, request, format=None):
+        health_id = request.data.get("health_id")
+        if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
+            if (app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().
+                    check_if_abha_registered(request.user, health_id)):
+                return generate_invalid_req_response(ABHA_ERROR_MESSAGES[ABHA_IN_USE_ERROR_CODE],
+                                                     error_code=ABHA_IN_USE_ERROR_CODE)
+        resp = abdm_util.search_by_health_id(health_id)
+        return parse_response(resp)
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["user_token"])
-def get_health_card_png(request):
-    user_token = request.data.get("user_token")
-    return parse_response(abdm_util.get_health_card_png(user_token))
+class GetHealthCardPng(ABHABaseView):
+
+    @method_decorator(required_request_params(["user_token"]))
+    def post(self, request, format=None):
+        user_token = request.data.get("user_token")
+        return parse_response(abdm_util.get_health_card_png(user_token))
 
 
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-@authentication_classes((app_settings.AUTHENTICATION_CLASS,))
-@required_request_params(["health_id"])
-def get_existence_by_health_id(request):
-    health_id = request.data.get("health_id")
-    if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
-        if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().check_if_abha_registered(request.user, health_id):
-            return generate_invalid_req_response(ABHA_ERROR_MESSAGES[ABHA_IN_USE_ERROR_CODE],
-                                                 error_code=ABHA_IN_USE_ERROR_CODE)
-    resp = abdm_util.exists_by_health_id(health_id)
-    if "status" in resp:
-        resp = {"health_id": health_id, "exists": resp.get("status")}
-    return parse_response(resp)
+class GetExistenceByHealthId(ABHABaseView):
+
+    @method_decorator(required_request_params(["health_id"]))
+    def post(self, request, format=None):
+        health_id = request.data.get("health_id")
+        if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS is not None:
+            if app_settings.HRP_ABHA_REGISTERED_CHECK_CLASS().check_if_abha_registered(request.user, health_id):
+                return generate_invalid_req_response(ABHA_ERROR_MESSAGES[ABHA_IN_USE_ERROR_CODE],
+                                                     error_code=ABHA_IN_USE_ERROR_CODE)
+        resp = abdm_util.exists_by_health_id(health_id)
+        if "status" in resp:
+            resp = {"health_id": health_id, "exists": resp.get("status")}
+        return parse_response(resp)

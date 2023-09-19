3e513033d14a00c1a27d4293e11c2a462427c63d
Ajeet Yadav <ayadav@dimagi.com>
2023-09-19 14:00:15 +0530

    add urls only for installed apps

--- abdm_integrator/urls.py
+++ abdm_integrator/urls.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+
 from abdm_integrator.abha.urls import abha_urls
 
-urlpatterns = abha_urls
+urlpatterns = []
+
+if 'abdm_integrator.abha' in settings.INSTALLED_APPS:
+    urlpatterns += abha_urls

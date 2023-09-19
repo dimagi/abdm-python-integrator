4adca48230d24e8aaa59d60103876298a8c93c61
Ajeet Yadav <ayadav@dimagi.com>
2023-09-19 20:28:09 +0530

    feedback: rename file names in abha views

--- abdm_python_integrator/abha/urls.py
+++ abdm_python_integrator/abha/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path
 
-from abdm_python_integrator.abha.views import abha_creation_views, abha_verification_views
+from abdm_python_integrator.abha.views import abha_creation, abha_verification
 
 abha_creation_urls = [
-    path('api/generate_aadhaar_otp', abha_creation_views.GenerateAadhaarOTP.as_view(),
+    path('api/generate_aadhaar_otp', abha_creation.GenerateAadhaarOTP.as_view(),
          name='generate_aadhaar_otp'),
-    path('api/generate_mobile_otp', abha_creation_views.GenerateMobileOTP.as_view(), name='generate_mobile_otp'),
-    path('api/verify_aadhaar_otp', abha_creation_views.VerifyAadhaarOTP.as_view(), name='verify_aadhaar_otp'),
-    path('api/verify_mobile_otp', abha_creation_views.VerifyMobileOTP.as_view(), name='verify_mobile_otp'),
+    path('api/generate_mobile_otp', abha_creation.GenerateMobileOTP.as_view(), name='generate_mobile_otp'),
+    path('api/verify_aadhaar_otp', abha_creation.VerifyAadhaarOTP.as_view(), name='verify_aadhaar_otp'),
+    path('api/verify_mobile_otp', abha_creation.VerifyMobileOTP.as_view(), name='verify_mobile_otp'),
 ]
 
 abha_verification_urls = [
-    path('api/get_auth_methods', abha_verification_views.GetAuthMethods.as_view(), name='get_auth_methods'),
-    path('api/generate_auth_otp', abha_verification_views.GenerateAuthOTP.as_view(), name='generate_auth_otp'),
-    path('api/confirm_with_mobile_otp', abha_verification_views.ConfirmWithMobileOTP.as_view(),
+    path('api/get_auth_methods', abha_verification.GetAuthMethods.as_view(), name='get_auth_methods'),
+    path('api/generate_auth_otp', abha_verification.GenerateAuthOTP.as_view(), name='generate_auth_otp'),
+    path('api/confirm_with_mobile_otp', abha_verification.ConfirmWithMobileOTP.as_view(),
          name='confirm_with_mobile_otp'),
-    path('api/confirm_with_aadhaar_otp', abha_verification_views.ConfirmWithAadhaarOTP.as_view(),
+    path('api/confirm_with_aadhaar_otp', abha_verification.ConfirmWithAadhaarOTP.as_view(),
          name='confirm_with_aadhaar_otp'),
-    path('api/search_health_id', abha_verification_views.SearchHealthId.as_view(), name='search_health_id'),
-    path('api/get_health_card_png', abha_verification_views.GetHealthCardPng.as_view(),
+    path('api/search_health_id', abha_verification.SearchHealthId.as_view(), name='search_health_id'),
+    path('api/get_health_card_png', abha_verification.GetHealthCardPng.as_view(),
          name='get_health_card_png'),
-    path('api/exists_by_health_id', abha_verification_views.GetExistenceByHealthId.as_view(),
+    path('api/exists_by_health_id', abha_verification.GetExistenceByHealthId.as_view(),
          name='exists_by_health_id'),
 ]
 

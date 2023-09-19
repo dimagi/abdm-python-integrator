d2d1e57928e4c96f9c245357451a9a6ddcd4720c
Ajeet Yadav <ayadav@dimagi.com>
2023-09-19 20:19:10 +0530

    removed not needed database settings attributes for test

--- test_settings.py
+++ test_settings.py
@@ -5,10 +5,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
     }
 }
 

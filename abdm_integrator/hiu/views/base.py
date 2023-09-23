b5e6a6100bcd1c8f6051b936d1633d719d18227e
Ajeet Yadav <ayadav@dimagi.com>
2023-09-23 22:01:19 +0530

    hiu base view for gateway facing apis

--- abdm_integrator/hiu/views/base.py
+++ abdm_integrator/hiu/views/base.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-from abdm_integrator.hiu.exceptions import hiu_error_response_handler
+from abdm_integrator.hiu.exceptions import hiu_error_response_handler, hiu_gateway_error_response_handler
 from abdm_integrator.settings import app_settings
 
 
@@ -11,3 +11,9 @@ class HIUBaseView(APIView):
 
     def get_exception_handler(self):
         return hiu_error_response_handler.get_exception_handler()
+
+
+class HIUGatewayBaseView(APIView):
+
+    def get_exception_handler(self):
+        return hiu_gateway_error_response_handler.get_exception_handler()

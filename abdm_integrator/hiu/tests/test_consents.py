da32e37435cb17af9b8bf93a6780ca49bf77b065
Ajeet Yadav <ayadav@dimagi.com>
2023-09-25 22:25:09 +0530

    HIU: test cases for consents and artefacts, refactor generate to use mixin

--- abdm_integrator/hiu/tests/test_consents.py
+++ abdm_integrator/hiu/tests/test_consents.py
@@ -1,17 +1,18 @@
 import json
 import os
+import uuid
 from copy import deepcopy
 from datetime import datetime, timedelta
 from unittest.mock import Mock, patch
 
 import requests
 from django.contrib.auth.models import User
-from rest_framework.exceptions import NotAuthenticated
+from django.utils.dateparse import parse_date
 from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.test import APIClient, APITestCase
 
-from abdm_integrator.const import ConsentStatus
+from abdm_integrator.const import ConsentStatus, HealthInformationType
 from abdm_integrator.exceptions import (
     ERROR_CODE_INVALID,
     ERROR_CODE_REQUIRED,
@@ -19,14 +20,14 @@ from abdm_integrator.exceptions import (
     ERROR_FUTURE_DATE_MESSAGE,
     STANDARD_ERRORS,
     ABDMGatewayError,
-    ABDMServiceUnavailable,
 )
 from abdm_integrator.hiu.exceptions import HIUError
-from abdm_integrator.hiu.models import ConsentRequest
+from abdm_integrator.hiu.models import ConsentArtefact, ConsentRequest
+from abdm_integrator.tests.utils import ErrorResponseAssertMixin
 from abdm_integrator.utils import abdm_iso_to_datetime, json_from_file
 
 
-class TestGenerateConsentRequestAPI(APITestCase):
+class TestGenerateConsentRequestAPI(APITestCase, ErrorResponseAssertMixin):
     dir_path = os.path.dirname(os.path.abspath(__file__))
     consent_sample_json_path = os.path.join(dir_path, 'data/generate_consent_request_sample.json')
 
@@ -54,15 +55,6 @@ class TestGenerateConsentRequestAPI(APITestCase):
         mock_response.json = Mock(return_value=json_response)
         mock_response.headers = {'content-type': 'application/json'}
         return mock_response
-
-    def _assert_error(self, actual_error, expected_code, expected_message):
-        self.assertEqual(actual_error['code'], expected_code)
-        self.assertEqual(actual_error['message'], expected_message)
-
-    def _assert_error_details(self, actual_error_details, expected_code, expected_detail, expected_attr):
-        self.assertEqual(actual_error_details['code'], expected_code)
-        self.assertEqual(actual_error_details['detail'], expected_detail)
-        self.assertEqual(actual_error_details['attr'], expected_attr)
 
     @classmethod
     def _sample_generate_consent_data(cls):
@@ -94,12 +86,7 @@ class TestGenerateConsentRequestAPI(APITestCase):
         request_data = self._sample_generate_consent_data()
         res = client.post(reverse('generate_consent_request'), data=json.dumps(request_data),
                           content_type='application/json')
-        json_res = res.json()
-        self.assertEqual(res.status_code, NotAuthenticated.status_code)
-        self._assert_error(json_res['error'], int(f'{HIUError.CODE_PREFIX}{NotAuthenticated.status_code}'),
-                           STANDARD_ERRORS.get(NotAuthenticated.status_code))
-        self._assert_error_details(json_res['error']['details'][0], NotAuthenticated.default_code,
-                                   NotAuthenticated.default_detail, None)
+        self.assert_for_authentication_error(res, HIUError.CODE_PREFIX)
         self.assertEqual(ConsentRequest.objects.all().count(), 0)
 
     def test_generate_consent_request_validation_error(self, *args):
@@ -110,13 +97,13 @@ class TestGenerateConsentRequestAPI(APITestCase):
                                content_type='application/json')
         json_res = res.json()
         self.assertEqual(res.status_code, HTTP_400_BAD_REQUEST)
-        self._assert_error(json_res['error'], int(f'{HIUError.CODE_PREFIX}{HTTP_400_BAD_REQUEST}'),
-                           STANDARD_ERRORS.get(HTTP_400_BAD_REQUEST))
+        self.assert_error(json_res['error'], int(f'{HIUError.CODE_PREFIX}{HTTP_400_BAD_REQUEST}'),
+                          STANDARD_ERRORS.get(HTTP_400_BAD_REQUEST))
         self.assertEqual(len(json_res['error']['details']), 2)
-        self._assert_error_details(json_res['error']['details'][0], ERROR_CODE_REQUIRED,
-                                   ERROR_CODE_REQUIRED_MESSAGE, 'patient.id')
-        self._assert_error_details(json_res['error']['details'][1], ERROR_CODE_INVALID,
-                                   ERROR_FUTURE_DATE_MESSAGE, 'permission.dataEraseAt')
+        self.assert_error_details(json_res['error']['details'][0], ERROR_CODE_REQUIRED,
+                                  ERROR_CODE_REQUIRED_MESSAGE, 'patient.id')
+        self.assert_error_details(json_res['error']['details'][1], ERROR_CODE_INVALID,
+                                  ERROR_FUTURE_DATE_MESSAGE, 'permission.dataEraseAt')
         self.assertEqual(ConsentRequest.objects.all().count(), 0)
 
     @patch('abdm_integrator.utils.ABDMRequestHelper.abha_post', return_value={'status': False})
@@ -126,10 +113,10 @@ class TestGenerateConsentRequestAPI(APITestCase):
                                content_type='application/json')
         self.assertEqual(res.status_code, HTTP_400_BAD_REQUEST)
         json_res = res.json()
-        self._assert_error(json_res['error'], HIUError.CODE_PATIENT_NOT_FOUND,
-                           HIUError.CUSTOM_ERRORS[HIUError.CODE_PATIENT_NOT_FOUND])
-        self._assert_error_details(json_res['error']['details'][0], ERROR_CODE_INVALID,
-                                   HIUError.CUSTOM_ERRORS[HIUError.CODE_PATIENT_NOT_FOUND], 'patient.id')
+        self.assert_error(json_res['error'], HIUError.CODE_PATIENT_NOT_FOUND,
+                          HIUError.CUSTOM_ERRORS[HIUError.CODE_PATIENT_NOT_FOUND])
+        self.assert_error_details(json_res['error']['details'][0], ERROR_CODE_INVALID,
+                                  HIUError.CUSTOM_ERRORS[HIUError.CODE_PATIENT_NOT_FOUND], 'patient.id')
         self.assertEqual(ConsentRequest.objects.all().count(), 0)
 
     @patch('abdm_integrator.utils.requests.post')
@@ -141,9 +128,9 @@ class TestGenerateConsentRequestAPI(APITestCase):
                                content_type='application/json')
         json_res = res.json()
         self.assertEqual(res.status_code, ABDMGatewayError.status_code)
-        self._assert_error(json_res['error'], gateway_error['error']['code'], ABDMGatewayError.error_message)
-        self._assert_error_details(json_res['error']['details'][0], ABDMGatewayError.detail_code,
-                                   gateway_error['error']['message'], None)
+        self.assert_error(json_res['error'], gateway_error['error']['code'], ABDMGatewayError.error_message)
+        self.assert_error_details(json_res['error']['details'][0], ABDMGatewayError.detail_code,
+                                  gateway_error['error']['message'], None)
         self.assertEqual(ConsentRequest.objects.all().count(), 0)
 
     @patch('abdm_integrator.utils.requests.post', side_effect=requests.Timeout)
@@ -153,10 +140,192 @@ class TestGenerateConsentRequestAPI(APITestCase):
         request_data = self._sample_generate_consent_data()
         res = client.post(reverse('generate_consent_request'), data=json.dumps(request_data),
                           content_type='application/json')
-        json_res = res.json()
-        self.assertEqual(res.status_code, ABDMServiceUnavailable.status_code)
-        self._assert_error(json_res['error'], int(f'{HIUError.CODE_PREFIX}{ABDMServiceUnavailable.status_code}'),
-                           STANDARD_ERRORS.get(ABDMServiceUnavailable.status_code))
-        self._assert_error_details(json_res['error']['details'][0], ABDMServiceUnavailable.default_code,
-                                   ABDMServiceUnavailable.default_detail, None)
-        self.assertEqual(ConsentRequest.objects.all().count(), 0)
+        self.assert_for_abdm_service_unavailable_error(res, HIUError.CODE_PREFIX)
+
+
+class TestListConsentsAndArtefactsAPI(APITestCase, ErrorResponseAssertMixin):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_superuser(username='test_user', password='test')
+        cls.consent_request_id_1 = str(uuid.uuid4())
+        cls.consent_request_id_2 = str(uuid.uuid4())
+        cls.patient_1 = 'test1@sbx'
+        cls.patient_2 = 'test2@sbx'
+        cls._add_consents_data()
+
+    @classmethod
+    def _add_consents_data(cls):
+        consent_data_1 = {
+            'consent_request_id': cls.consent_request_id_1,
+            'gateway_request_id': str(uuid.uuid4()),
+            'status': ConsentStatus.GRANTED,
+            'health_info_from_date': '2011-05-17T15:12:43.960000',
+            'health_info_to_date': '2017-08-07T15:12:43.961000',
+            'health_info_types': [
+                HealthInformationType.DISCHARGE_SUMMARY,
+                HealthInformationType.PRESCRIPTION,
+                HealthInformationType.WELLNESS_RECORD
+            ],
+            'expiry_date': (datetime.utcnow() + timedelta(days=1)).isoformat(),
+            'details': {
+                'patient': {
+                    'id': cls.patient_1
+                }
+            }
+        }
+        consent_request_1 = ConsentRequest.objects.create(**consent_data_1, user=cls.user)
+        artefact_data_1 = {
+            'artefact_id': str(uuid.uuid4()),
+            'gateway_request_id': str(uuid.uuid4()),
+            'details': {
+                'hip': {
+                    'id': '6004',
+                    'name': 'Test Eye Care Center '
+                },
+            }
+        }
+        artefact_data_2 = {
+            'artefact_id': str(str(uuid.uuid4())),
+            'gateway_request_id': str(str(uuid.uuid4())),
+            'details': {
+                'hip': {
+                    'id': '6005',
+                    'name': 'Demo Clinic'
+                },
+            }
+        }
+        ConsentArtefact.objects.create(**artefact_data_1, consent_request=consent_request_1)
+        ConsentArtefact.objects.create(**artefact_data_2, consent_request=consent_request_1)
+        consent_data_2 = {
+            'consent_request_id': cls.consent_request_id_2,
+            'gateway_request_id': str(str(uuid.uuid4())),
+            'status': ConsentStatus.EXPIRED,
+            'health_info_from_date': '2021-05-17T15:12:43.960000',
+            'health_info_to_date': '2023-08-07T15:12:43.961000',
+            'health_info_types': [
+                "WellnessRecord"
+            ],
+            'expiry_date': (datetime.utcnow() - timedelta(days=2)).isoformat(),
+            'details': {
+                'patient': {
+                    'id': cls.patient_2
+                }
+            }
+        }
+        ConsentRequest.objects.create(**consent_data_2, user=cls.user)
+
+    def setUp(self):
+        self.client.force_authenticate(user=self.user)
+
+    @classmethod
+    def tearDownClass(cls):
+        ConsentArtefact.objects.all().delete()
+        ConsentRequest.objects.all().delete()
+        User.objects.all().delete()
+        super().tearDownClass()
+
+    def test_list_consents(self):
+        res = self.client.get(reverse('consents_list'))
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 2)
+
+    def test_list_consents_authentication_error(self):
+        res = APIClient().get(reverse('consents_list'))
+        self.assert_for_authentication_error(res, HIUError.CODE_PREFIX)
+
+    def test_list_consents_filter_status(self):
+        params = {"status": ConsentStatus.EXPIRED}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertEqual(res.json()["results"][0]["status"], params["status"])
+
+    def test_list_consents_filter_abha_id(self):
+        params = {"abha_id": self.patient_1}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertEqual(res.json()["results"][0]["details"]["patient"]["id"], params["abha_id"])
+
+    def test_list_consents_filter_search(self):
+        params = {"search": "prescript"}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertTrue(any(params["search"].casefold() in hi_type.casefold()
+                            for hi_type in res.json()["results"][0]["health_info_types"]))
+
+    def test_list_consents_filter_from_date(self):
+        params = {"from_date": "2018-07-10"}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertGreaterEqual(abdm_iso_to_datetime(
+            res.json()["results"][0]["health_info_to_date"]).date(),
+            parse_date(params["from_date"])
+        )
+
+    def test_list_consents_filter_to_date(self):
+        params = {"to_date": "2016-05-18"}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertLessEqual(abdm_iso_to_datetime(res.json()["results"][0]["health_info_from_date"]).date(),
+                             parse_date(params["to_date"]))
+
+    def test_list_consents_filter_from_and_to_date(self):
+        params = {"from_date": "2015-07-10", "to_date": "2022-05-18"}
+        res = self.client.get(reverse('consents_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 2)
+        for result in res.json()["results"]:
+            self.assertGreaterEqual(abdm_iso_to_datetime(result["health_info_to_date"]).date(),
+                                    parse_date(params["from_date"]))
+            self.assertLessEqual(abdm_iso_to_datetime(result["health_info_from_date"]).date(),
+                                 parse_date(params["to_date"]))
+
+    def test_retrieve_consent(self):
+        res = self.client.get(f"{reverse('consents_retrieve', args=['1'])}")
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()['consent_request_id'], self.consent_request_id_1)
+
+    def test_retrieve_consent_authentication_error(self):
+        res = APIClient().get(f"{reverse('consents_retrieve', args=['1'])}")
+        self.assert_for_authentication_error(res, HIUError.CODE_PREFIX)
+
+    def test_list_consent_artefacts_consent_id(self):
+        params = {"consent_request_id": self.consent_request_id_1}
+        res = self.client.get(reverse('artefacts_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 2)
+        for result in res.json()["results"]:
+            self.assertEqual(result["consent_request"], params['consent_request_id'])
+
+    def test_list_consent_artefacts_authentication_error(self):
+        res = APIClient().get(reverse('artefacts_list'))
+        self.assert_for_authentication_error(res, HIUError.CODE_PREFIX)
+
+    def test_list_consent_artefacts_no_consent_id(self):
+        res = self.client.get(reverse('artefacts_list'))
+        self.assertEqual(res.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.json()['error']['details'][0],
+                         {'attr': 'consent_request_id', 'code': ERROR_CODE_REQUIRED,
+                          'detail': ERROR_CODE_REQUIRED_MESSAGE})
+
+    def test_list_consents_artefacts_search(self):
+        params = {"consent_request_id": self.consent_request_id_1, "search": "test eye"}
+        res = self.client.get(reverse('artefacts_list'), data=params)
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()["count"], 1)
+        self.assertIn(params["search"].casefold(), res.json()["results"][0]["details"]["hip"]["name"].casefold())
+
+    def test_retrieve_consent_artefact(self):
+        res = self.client.get(f"{reverse('artefacts_retrieve', args=['1'])}")
+        self.assertEqual(res.status_code, HTTP_200_OK)
+        self.assertEqual(res.json()['consent_request'], self.consent_request_id_1)
+
+    def test_retrieve_consent_artefact_authentication_error(self):
+        res = APIClient().get(f"{reverse('artefacts_retrieve', args=['1'])}")
+        self.assert_for_authentication_error(res, HIUError.CODE_PREFIX)

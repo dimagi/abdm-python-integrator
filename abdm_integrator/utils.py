15699123420cca1e42db188cbee392326c07c6ce
Ajeet Yadav <ayadav@dimagi.com>
2023-09-26 20:40:05 +0530

    abdm network call- sets default timeout

--- abdm_integrator/utils.py
+++ abdm_integrator/utils.py
@@ -21,6 +21,7 @@ class ABDMRequestHelper:
     gateway_base_url = app_settings.GATEWAY_URL
     abha_base_url = app_settings.ABHA_URL
     token_payload = {"clientId": app_settings.CLIENT_ID, "clientSecret": app_settings.CLIENT_SECRET}
+    default_timeout = 60
 
     def __init__(self):
         self.headers = {'Content-Type': "application/json", 'X-CM-ID': app_settings.X_CM_ID}
@@ -29,7 +30,7 @@ class ABDMRequestHelper:
         headers = {"Content-Type": "application/json; charset=UTF-8"}
         try:
             resp = requests.post(url=self.gateway_base_url + SESSIONS_PATH, data=json.dumps(self.token_payload),
-                                 headers=headers)
+                                 headers=headers, timeout=self.default_timeout)
             resp.raise_for_status()
         except requests.Timeout:
             raise ABDMServiceUnavailable()
@@ -38,29 +39,31 @@ class ABDMRequestHelper:
             raise ABDMGatewayError(error.get('code'), error.get('message'))
         return resp.json().get("accessToken")
 
-    def abha_get(self, api_path, additional_headers=None, params=None):
+    def abha_get(self, api_path, additional_headers=None, params=None, timeout=None):
         self.headers.update({"Authorization": f"Bearer {self.get_access_token()}"})
         if additional_headers:
             self.headers.update(additional_headers)
-        resp = requests.get(url=self.abha_base_url + api_path, headers=self.headers, params=params)
+        resp = requests.get(url=self.abha_base_url + api_path, headers=self.headers, params=params,
+                            timeout=timeout or self.default_timeout)
         resp.raise_for_status()
         # ABHA APIS may not return 'application/json' content type in headers as per swagger doc
         return _get_json_from_resp(resp)
 
-    def _post(self, url, payload):
+    def _post(self, url, payload, timeout=None):
         self.headers.update({"Authorization": f"Bearer {self.get_access_token()}"})
-        resp = requests.post(url=url, headers=self.headers, data=json.dumps(payload))
+        resp = requests.post(url=url, headers=self.headers, data=json.dumps(payload),
+                             timeout=timeout or self.default_timeout)
         resp.raise_for_status()
         return resp
 
-    def abha_post(self, api_path, payload):
-        resp = self._post(self.abha_base_url + api_path, payload)
+    def abha_post(self, api_path, payload, timeout=None):
+        resp = self._post(self.abha_base_url + api_path, payload, timeout)
         # ABHA APIS may not return 'application/json' content type in headers as per swagger doc
         return _get_json_from_resp(resp)
 
-    def gateway_post(self, api_path, payload):
+    def gateway_post(self, api_path, payload, timeout=None):
         try:
-            resp = self._post(self.gateway_base_url + api_path, payload)
+            resp = self._post(self.gateway_base_url + api_path, payload, timeout)
         except requests.Timeout:
             raise ABDMServiceUnavailable()
         except requests.HTTPError as err:

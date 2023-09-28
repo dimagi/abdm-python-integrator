bb0ef4abc90b0086ce2d2ae03b21d425e951d674
Ajeet Yadav <ayadav@dimagi.com>
2023-09-28 16:28:45 +0530

    HIP: add urls to urlpatterns

--- abdm_integrator/urls.py
+++ abdm_integrator/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from abdm_integrator.abha.urls import abha_urls
+from abdm_integrator.hip.urls import hip_urls
 from abdm_integrator.hiu.urls import hiu_urls
 
 urlpatterns = []
@@ -9,3 +10,5 @@ if 'abdm_integrator.abha' in settings.INSTALLED_APPS:
     urlpatterns += abha_urls
 if 'abdm_integrator.hiu' in settings.INSTALLED_APPS:
     urlpatterns += hiu_urls
+if 'abdm_integrator.hip' in settings.INSTALLED_APPS:
+    urlpatterns += hip_urls

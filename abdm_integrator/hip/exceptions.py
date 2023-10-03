198de603daf041f6c24030e267c007a7092960e6
Ajeet Yadav <ayadav@dimagi.com>
2023-10-03 22:55:16 +0530

    API for care context link

--- abdm_integrator/hip/exceptions.py
+++ abdm_integrator/hip/exceptions.py
@@ -4,7 +4,10 @@ from abdm_integrator.exceptions import APIErrorResponseHandler
 class HIPError:
     CODE_PREFIX = '3'
 
-    CUSTOM_ERRORS = {}
+    CODE_CARE_CONTEXT_ALREADY_LINKED = 3420
+    CUSTOM_ERRORS = {
+        CODE_CARE_CONTEXT_ALREADY_LINKED: '{} care contexts are already linked'
+    }
 
 
 hip_error_response_handler = APIErrorResponseHandler(HIPError.CODE_PREFIX)
--- abdm_integrator/hip/urls.py
+++ abdm_integrator/hip/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from abdm_integrator.const import GATEWAY_CALLBACK_URL_PREFIX
+from abdm_integrator.hip.views.care_contexts import GatewayOnAddContexts, LinkCareContextRequest
 from abdm_integrator.hip.views.consents import GatewayConsentRequestNotify
 
 hip_urls = [
+    path('api/hip/link_care_context', LinkCareContextRequest.as_view(), name='link_care_context'),
     # APIS that will be triggered by ABDM Gateway
     path(f'{GATEWAY_CALLBACK_URL_PREFIX}/consents/hip/notify', GatewayConsentRequestNotify.as_view(),
          name='gateway_consent_request_notify_hip'),
+    path(f'{GATEWAY_CALLBACK_URL_PREFIX}/links/link/on-add-contexts', GatewayOnAddContexts.as_view(),
+         name='gateway_on_add_contexts'),
 ]

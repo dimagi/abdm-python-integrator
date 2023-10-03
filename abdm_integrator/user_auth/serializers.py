d396cf0837229f1d9b81f83877512688e6d9eb30
Ajeet Yadav <ayadav@dimagi.com>
2023-10-03 20:45:10 +0530

    API for auth init

--- abdm_integrator/user_auth/serializers.py
+++ abdm_integrator/user_auth/serializers.py
@@ -22,3 +22,35 @@ class GatewayAuthOnFetchModesSerializer(GatewayCallbackResponseBaseSerializer):
         modes = serializers.ListField(child=serializers.ChoiceField(choices=AuthenticationMode.CHOICES))
 
     auth = AuthSerializer(required=False, allow_null=True)
+
+
+class AuthRequesterSerializer(serializers.Serializer):
+    type = serializers.ChoiceField(choices=RequesterType.CHOICES)
+    id = serializers.CharField()
+
+
+class AuthInitSerializer(serializers.Serializer):
+
+    id = serializers.CharField()
+    purpose = serializers.ChoiceField(choices=AuthFetchModesPurpose.CHOICES)
+    requester = AuthRequesterSerializer()
+    authMode = serializers.ChoiceField(choices=AuthenticationMode.CHOICES, required=False)
+
+    def validate_authMode(self, data):
+        if data == AuthenticationMode.DIRECT:
+            raise serializers.ValidationError(f"'{AuthenticationMode.DIRECT}' Auth mode is not supported!")
+
+
+class GatewayAuthOnInitSerializer(GatewayCallbackResponseBaseSerializer):
+
+    class AuthSerializer(serializers.Serializer):
+
+        class MetaSerializer(serializers.Serializer):
+            hint = serializers.CharField(allow_null=True)
+            expiry = serializers.CharField()
+
+        transactionId = serializers.CharField()
+        mode = serializers.ChoiceField(choices=AuthenticationMode.CHOICES)
+        meta = MetaSerializer(required=False)
+
+    auth = AuthSerializer(required=False, allow_null=True)

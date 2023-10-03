4987bdfe2623f71cc4d3e1d56b60addd6df77747
Ajeet Yadav <ayadav@dimagi.com>
2023-10-03 19:46:47 +0530

    constants related to user auth

---
+++ abdm_integrator/user_auth/const.py
@@ -0,0 +1,9 @@
+
+class UserAuthGatewayAPIPath:
+    FETCH_AUTH_MODES = "/v0.5/users/auth/fetch-modes"
+    AUTH_INIT = "/v0.5/users/auth/init"
+    AUTH_CONFIRM = "/v0.5/users/auth/confirm"
+
+
+IDENTIFIER_TYPE_CHOICES = ["MR", "MOBILE", "NDHM_HEALTH_NUMBER", "HEALTH_ID", "EMAIL"]
+GENDER_CHOICES = ['M', 'F', 'O', 'U']

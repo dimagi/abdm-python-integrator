964c845ef6d15116e0b539aae9dabf68e16e204e
Ajeet <ayadav@dimagi.com>
2023-11-07 18:43:51 +0530

    nit: styling, rename methods for HIP health information

--- abdm_integrator/hip/views/health_information.py
+++ abdm_integrator/hip/views/health_information.py
@@ -6,7 +6,12 @@ import requests
 from rest_framework.response import Response
 from rest_framework.status import HTTP_202_ACCEPTED
 
-from abdm_integrator.const import HEALTH_INFORMATION_MEDIA_TYPE, HealthInformationStatus, LinkRequestStatus
+from abdm_integrator.const import (
+    HEALTH_INFORMATION_MEDIA_TYPE,
+    HealthInformationStatus,
+    LinkRequestStatus,
+    RequesterType,
+)
 from abdm_integrator.crypto import ABDMCrypto
 from abdm_integrator.exceptions import ABDMGatewayError, ABDMServiceUnavailable
 from abdm_integrator.hip.const import HIPGatewayAPIPath
@@ -38,7 +43,8 @@ class GatewayHealthInformationRequestProcessor:
     def __init__(self, request_data):
         self.request_data = request_data
         self.health_information_request = HealthInformationRequest.objects.create(
-            transaction_id=self.request_data['transactionId'])
+            transaction_id=self.request_data['transactionId']
+        )
 
     def process_request(self):
         artefact = self.fetch_artefact()
@@ -52,8 +58,9 @@ class GatewayHealthInformationRequestProcessor:
             self.update_health_information_request_error(error)
             return error
         overall_transfer_status, care_contexts_status = HealthDataTransferProcessor(
-            self.health_information_request, self.request_data['hiRequest']).process()
-        self.update_transfer_status(overall_transfer_status)
+            self.health_information_request, self.request_data['hiRequest']
+        ).process()
+        self.update_health_information_request_transfer_status(overall_transfer_status)
         self.gateway_health_information_on_transfer(overall_transfer_status, care_contexts_status)
 
     def fetch_artefact(self):
@@ -108,27 +115,39 @@ class GatewayHealthInformationRequestProcessor:
         if error:
             payload['error'] = error
         else:
-            payload['hiRequest'] = {'transactionId': self.request_data['transactionId'],
-                                    'sessionStatus': HealthInformationStatus.ACKNOWLEDGED}
+            payload['hiRequest'] = {
+                'transactionId': self.request_data['transactionId'],
+                'sessionStatus': HealthInformationStatus.ACKNOWLEDGED
+            }
         payload['resp'] = {'requestId': self.request_data['requestId']}
         ABDMRequestHelper().gateway_post(HIPGatewayAPIPath.HEALTH_INFO_ON_REQUEST, payload)
         return payload['requestId']
 
-    def update_transfer_status(self, overall_transfer_status):
-        self.health_information_request.status = (HealthInformationStatus.TRANSFERRED
-                                                  if overall_transfer_status else HealthInformationStatus.FAILED)
+    def update_health_information_request_transfer_status(self, overall_transfer_status):
+        self.health_information_request.status = (
+            HealthInformationStatus.TRANSFERRED if overall_transfer_status else HealthInformationStatus.FAILED
+        )
         self.health_information_request.save()
 
-    def gateway_health_information_on_transfer(self, transfer_status, care_contexts_status):
+    def gateway_health_information_on_transfer(self, overall_transfer_status, care_contexts_status):
         artefact = self.health_information_request.consent_artefact
+        session_status = (
+            HealthInformationStatus.TRANSFERRED if overall_transfer_status else HealthInformationStatus.FAILED
+        )
         payload = ABDMRequestHelper.common_request_data()
-        payload['notification'] = {'consent_id': str(artefact.artefact_id),
-                                   'transaction_id': self.request_data['transactionId'],
-                                   'doneAt': datetime.utcnow().isoformat()}
-        payload['notification']['notifier'] = {'type': 'HIP', 'id': artefact.details['hip']['id']}
-        session_status = HealthInformationStatus.TRANSFERRED if transfer_status else HealthInformationStatus.FAILED
-        payload['notification']['statusNotification'] = {'sessionStatus': session_status,
-                                                         'hipId': artefact.details['hip']['id']}
+        payload['notification'] = {
+            'consent_id': str(artefact.artefact_id),
+            'transaction_id': self.request_data['transactionId'],
+            'doneAt': datetime.utcnow().isoformat(),
+            'notifier': {
+                'type': RequesterType.HIP,
+                'id': artefact.details['hip']['id']
+            },
+            'statusNotification': {
+                'sessionStatus': session_status,
+                'hipId': artefact.details['hip']['id']
+            }
+        }
         # Avoids sending description for care context as optional and could possibly
         # contain internal error details. Could be included in case a need/use case arises later on.
         for care_context_status in care_contexts_status:
@@ -150,8 +169,9 @@ class HealthDataTransferProcessor:
 
     def process(self):
         care_contexts_status = []
-        for index, care_contexts_chunks in enumerate(self._generate_chunks(self.care_contexts,
-                                                                           self.entries_per_page)):
+        for index, care_contexts_chunks in enumerate(
+            self._generate_chunks(self.care_contexts, self.entries_per_page)
+        ):
             care_contexts_chunks_status = self._process_page(index + 1, care_contexts_chunks)
             care_contexts_status.extend(care_contexts_chunks_status)
         overall_transfer_status = not any(status['hiStatus'] == HealthInformationStatus.ERRORED
@@ -163,8 +183,13 @@ class HealthDataTransferProcessor:
         return int(math.ceil(len(self.care_contexts) / self.entries_per_page))
 
     def _process_page(self, page_number, care_contexts):
-        payload = {'pageCount': self.page_count, 'transactionId': self.health_information_request.transaction_id,
-                   'keyMaterial': self.crypto.transfer_material, 'pageNumber': page_number, 'entries': []}
+        payload = {
+            'pageCount': self.page_count,
+            'transactionId': self.health_information_request.transaction_id,
+            'keyMaterial': self.crypto.transfer_material,
+            'pageNumber': page_number,
+            'entries': []
+        }
         care_contexts_status = []
         care_contexts_transfer = []
 
@@ -190,17 +215,20 @@ class HealthDataTransferProcessor:
     def _process_care_context(self, care_context):
         entries = []
         linked_care_context = self.fetch_linked_care_context(care_context)
-        health_info_types = self.check_health_information_types(linked_care_context)
+        valid_health_info_types = self.validate_health_information_types(linked_care_context)
         self.validate_health_info_date_range(linked_care_context)
-        fhir_data = self.fetch_fhir_data_hrp(linked_care_context, health_info_types)
+        fhir_data = self.fetch_fhir_data_from_hrp(linked_care_context, valid_health_info_types)
         for bundle in fhir_data:
             encrypted_entry = self.get_encrypted_entry(care_context['careContextReference'], bundle)
             entries.append(encrypted_entry)
         return entries
 
     def save_health_data_transfer(self, page_number, care_contexts_status):
-        HealthDataTransfer.objects.create(health_information_request=self.health_information_request,
-                                          page_number=page_number, care_contexts_status=care_contexts_status)
+        HealthDataTransfer.objects.create(
+            health_information_request=self.health_information_request,
+            page_number=page_number,
+            care_contexts_status=care_contexts_status
+        )
 
     def fetch_linked_care_context(self, care_context):
         hip_id = self.health_information_request.consent_artefact.details['hip']['id']
@@ -212,17 +240,20 @@ class HealthDataTransferProcessor:
                 link_request_details__status=LinkRequestStatus.SUCCESS
             )
         except LinkCareContext.DoesNotExist:
-            raise HealthDataTransferException(f"Linked Care Context not found for "
-                                              f"{care_context['careContextReference']}")
+            raise HealthDataTransferException(
+                f"Linked Care Context not found for {care_context['careContextReference']}"
+            )
 
-    def check_health_information_types(self, linked_care_context):
+    def validate_health_information_types(self, linked_care_context):
         consented_health_info_types = self.health_information_request.consent_artefact.details['hiTypes']
-        health_info_types = list(set(consented_health_info_types).intersection(
-            linked_care_context.health_info_types))
-        if not health_info_types:
-            raise HealthDataTransferException(f'Validation failed for HI Types for care context: '
-                                              f'{linked_care_context.reference}')
-        return health_info_types
+        valid_health_info_types = list(
+            set(consented_health_info_types).intersection(linked_care_context.health_info_types)
+        )
+        if not valid_health_info_types:
+            raise HealthDataTransferException(
+                f'Validation failed for HI Types for care context: {linked_care_context.reference}'
+            )
+        return valid_health_info_types
 
     def validate_health_info_date_range(self, linked_care_context):
         health_record_date = linked_care_context.additional_info.get('record_date')
@@ -230,16 +261,20 @@ class HealthDataTransferProcessor:
             requested_from_date = abdm_iso_to_datetime(self.hi_request['dateRange']['from'])
             requested_to_date = abdm_iso_to_datetime(self.hi_request['dateRange']['to'])
             if not (requested_from_date <= abdm_iso_to_datetime(health_record_date) <= requested_to_date):
-                raise HealthDataTransferException(f'Health record date is not in requested date range for'
-                                                  f'{linked_care_context.reference}')
+                raise HealthDataTransferException(
+                    f'Health record date is not in requested date range for {linked_care_context.reference}'
+                )
 
-    def fetch_fhir_data_hrp(self, linked_care_context, health_info_types):
+    def fetch_fhir_data_from_hrp(self, linked_care_context, health_info_types):
         linked_care_context_serialized = LinkCareContextFetchSerializer(linked_care_context).data
         try:
-            fhir_data = (app_settings.HRP_INTEGRATION_CLASS().
-                         fetch_health_data(linked_care_context.reference,
-                                           health_info_types,
-                                           linked_care_context_serialized))
+            fhir_data = (
+                app_settings.HRP_INTEGRATION_CLASS().fetch_health_data(
+                    linked_care_context.reference,
+                    health_info_types,
+                    linked_care_context_serialized
+                )
+            )
         except Exception as err:
             raise HealthDataTransferException(f'Error occurred while fetching health data from HRP: {err}')
         return fhir_data
@@ -256,8 +291,12 @@ class HealthDataTransferProcessor:
 
     def send_data_to_hiu(self, payload):
         try:
-            resp = requests.post(url=self.hi_request['dataPushUrl'], data=json.dumps(payload),
-                                 headers={'Content-Type': 'application/json'}, timeout=60)
+            resp = requests.post(
+                url=self.hi_request['dataPushUrl'],
+                data=json.dumps(payload),
+                headers={'Content-Type': 'application/json'},
+                timeout=60
+            )
             resp.raise_for_status()
         except Exception as err:
             raise HealthDataTransferException(f'Error occurred while sending health data to HIU: {err}')
